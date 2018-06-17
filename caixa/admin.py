7436c9a206ffc144767d2780dc00c5d80a21f0ad
edimar <manicaedimar@gmail.com>
2018-06-17 20:48:20 -0300

    Estou fazendo a importação de caixas a partir do CSV. Não terminei

--- caixa/admin.py
+++ caixa/admin.py
@@ -1,6 +1,11 @@
+# Classes do django
 from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render, redirect 
 
+# Classes minhas
 from caixa.models import Tipo, Caixa
+from caixa.forms import CsvImportForm
 from functions.utils import DateUtils
 
 class CaixaAdmin(admin.ModelAdmin):
@@ -8,6 +13,8 @@ class CaixaAdmin(admin.ModelAdmin):
     list_filter = ('vencimento', 'pagamento')  #definindo os filtros
     search_fields = ['descricao', ]
     actions = ['proximo_mes', ]
+    
+    change_list_template = "caixa_changelist.html"
 
     def fluxo(self, obj):
         return obj.tipo.get_fluxo_display() # get_NOMEATRIBUTO_display() é uma função para pegar o valor dos choices
@@ -31,6 +38,31 @@ class CaixaAdmin(admin.ModelAdmin):
             obj.pagamento = None
             obj.save()
     proximo_mes.short_description = "Gerar próximo vencimento"
+    
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('import-csv/', self.import_csv),
+        ]
+        return my_urls + urls
+    
+    def import_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES["csv_file"]
+            
+            for line in csv_file:
+                msg = line.decode('iso-8859-1')+""
+            
+         #   reader = csv.reader(csv_file)
+            # Create Hero objects from passed in data
+            # ...
+            self.message_user(request, "Your csv file has been imported"+msg)
+            return redirect("..")
+        form = CsvImportForm()
+        payload = {"form": form}
+        return render(
+            request, "csv_form.html", payload
+        )
 
 admin.site.register(Tipo)
 admin.site.register(Caixa, CaixaAdmin)
